6802d941dcbf80b22dbec13b8dea81ef26afc9e6
Benjamin Croad <benjamincroadapple@gmail.com>
2022-08-23 15:44:25 +0100

    Solution to exercise 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Running seeds..."
+10.times do
+  Article.create(
+    title: Faker::FunnyName.name,
+    content: Faker::Hipster.sentence
+  )
+end
+puts "Database seeded..."
